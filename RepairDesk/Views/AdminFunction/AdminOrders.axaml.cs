539882cad4e858106e5575c407f35a5dbc4d2a5d
ivanlutsin <ivanlutsin3005080@gmail.com>
2026-04-04 18:04:17 +0300

    Y meniya gotovo okno Administratora

---
+++ RepairDesk/Views/AdminFunction/AdminOrders.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace RepairDesk.Views.AdminFunction;
+
+public partial class AdminOrders : Window
+{
+    public AdminOrders()
+    {
+        InitializeComponent();
+    }
+}
---
+++ RepairDesk/Views/AdminFunction/Devices.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace RepairDesk.Views.AdminFunction;
+
+public partial class Devices : Window
+{
+    public Devices()
+    {
+        InitializeComponent();
+    }
+}
