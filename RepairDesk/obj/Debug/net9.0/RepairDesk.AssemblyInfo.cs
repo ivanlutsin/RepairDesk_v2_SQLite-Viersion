9a7e733d48558043238941f1758fc8c49ec611c7
ivanlutsin <ivanlutsin3005080@gmail.com>
2026-06-06 23:54:20 +0300

    RepairDesk v3.4: Seal of the contract

--- RepairDesk/obj/Debug/net9.0/RepairDesk.AssemblyInfo.cs
+++ RepairDesk/obj/Debug/net9.0/RepairDesk.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RepairDesk")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba7ae5e7b4536d86ccf0d0ebf74fd3d018e89483")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19fd5244b8960819d29f69e9abf239db267a484d")]
 [assembly: System.Reflection.AssemblyProductAttribute("RepairDesk")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RepairDesk")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
